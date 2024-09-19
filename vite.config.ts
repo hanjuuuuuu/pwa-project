4862e405a9ba07e7d978d8d12ff7457797d7e914
김 한주 <hjkim@jinhaksa.net>
2024-09-19 14:55:31 +0900

    vite 플러그인을 통해 빌드 후 서비스워커 파일을  DIST폴더에에 복사

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'service-worker.js',
+          dest: ''
+        }
+      ]
+    })
+  ],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: "./index.html",
